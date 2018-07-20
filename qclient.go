2f105ebbf70d7a6aac4cffbb758296ca50a713ae
yzihua <yuanzihua0@gmail.com>
2018-07-20 11:14:38 +0800

    update api

--- qclient.go
+++ qclient.go
@@ -1,12 +1,20 @@
 package qclient
 
 import (
-    "github.com/qiniu/api.v7/auth/qbox"
-    "github.com/qiniu/api.v7/storage"
     "context"
     "io"
     "os"
     "bytes"
+    "time"
+    "net/http"
+    "io/ioutil"
+    "github.com/qiniu/api.v7/auth/qbox"
+    "github.com/qiniu/api.v7/storage"
+    "strings"
+    "errors"
+    "sync"
+    "path"
+    "strconv"
 )
 
 const (
@@ -21,6 +29,17 @@ type Stat struct {
     Name   string
 }
 
+type FileInfo struct {
+    Key      string `json:"key"`
+    Hash     string `json:"hash"`
+    Size     int64  `json:"size"`
+    PutTime  int64  `json:"putTime"`
+    MimeType string `json:"mimeType"`
+    Type     int    `json:"type"`
+    IsDir    bool   `json:"isDir"`
+    Name     string `json:"name"`
+}
+
 type QClient struct {
     accessKey  string
     secretKey  string
@@ -28,16 +47,20 @@ type QClient struct {
     domain     string
     endpoint   string
     config     *storage.Config
+    cbURL      string
     mac        *qbox.Mac
-    putPolicy  *storage.PutPolicy
-    uploader   *storage.ResumeUploader
     bktManager *storage.BucketManager
+    uploader   *storage.ResumeUploader
 }
 
 // cbURL=args[0],endpoint=args[1]
 func NewQClient(ak, sk, bucket, domain string, useHttps, useCDN bool, args ...string) (*QClient) {
+
     mac := qbox.NewMac(ak, sk)
-    var cbURL, endpoint string
+    var (
+        cbURL    string
+        endpoint string
+    )
     switch len(args) {
     case 2:
         cbURL, endpoint = args[0], args[1]
@@ -45,17 +68,12 @@ func NewQClient(ak, sk, bucket, domain string, useHttps, useCDN bool, args ...st
         cbURL = args[0]
     }
 
-    putPolicy := &storage.PutPolicy{
-        Scope:       bucket,
-        CallbackURL: cbURL,
-        ReturnBody:  retBody,
-    }
     cfg := storage.Config{
         UseHTTPS:      useHttps,
         UseCdnDomains: useCDN,
     }
-    uploader := storage.NewResumeUploader(&cfg)
     bktManager := storage.NewBucketManager(mac, &cfg)
+    uploader := storage.NewResumeUploader(&cfg)
 
     return &QClient{
         accessKey:  ak,
@@ -65,16 +83,14 @@ func NewQClient(ak, sk, bucket, domain string, useHttps, useCDN bool, args ...st
         endpoint:   endpoint,
         config:     &cfg,
         mac:        mac,
-        putPolicy:  putPolicy,
-        uploader:   uploader,
+        cbURL:      cbURL,
         bktManager: bktManager,
+        uploader:   uploader,
     }
+
 }
 
-func (q *QClient) upToken() string {
-    return q.putPolicy.UploadToken(q.mac)
-}
-
+// extra 用于构造上传到七牛云存储上的扩展名
 func (q *QClient) extra(kind string) storage.RputExtra {
     return storage.RputExtra{
         Params: map[string]string{
@@ -83,14 +99,54 @@ func (q *QClient) extra(kind string) storage.RputExtra {
     }
 }
 
-func (q *QClient) put(path string, reader io.ReaderAt, fSize int64) (*Stat, error) {
+// qPath 保证文件一定有前缀 "/"
+func (q *QClient) qPath(path string) string {
+    if !strings.HasPrefix(path, "/") {
+        path = "/" + path
+    }
+    return path
+
+}
+
+// pathBase 获取不包含路径的文件名
+func (q *QClient) pathBase(fName string) string {
+    return path.Base(fName)
+
+}
+
+func (q *QClient) URLFor(path string) string {
+    deadline := time.Now().Add(time.Hour * 3).Unix()
+    return storage.MakePrivateURL(q.mac, q.domain, path, deadline)
+}
+
+// Writer 负责把数据写入到七牛云存储
+func (q *QClient) Writer(path string, reader io.ReaderAt, fSize int64) (*Stat, error) {
+
+    policy := &storage.PutPolicy{
+        Scope:       q.bucket,
+        CallbackURL: q.cbURL,
+        ReturnBody:  retBody,
+    }
     ret := Stat{}
     extra := q.extra(path)
-    err := q.uploader.Put(context.Background(), &ret, q.upToken(), path, reader, fSize, &extra)
+    err := q.uploader.Put(
+        context.Background(),
+        &ret,
+        policy.UploadToken(q.mac),
+        q.qPath(path),
+        reader,
+        fSize,
+        &extra,
+    )
     return &ret, err
 }
 
-func (q *QClient) PutFile(filename string) (*Stat, error) {
+func (q *QClient) Push(path string, data []byte) (*Stat, error) {
+    stat, err := q.Writer(path, bytes.NewReader(data), int64(len(data)))
+    return stat, err
+}
+
+func (q *QClient) PushFile(filename string) (*Stat, error) {
     f, err := os.Open(filename)
     if err != nil {
         return nil, err
@@ -100,16 +156,168 @@ func (q *QClient) PutFile(filename string) (*Stat, error) {
     if err != nil {
         return nil, err
     }
-    ret, err := q.put(filename, f, info.Size())
+    ret, err := q.Writer(filename, f, info.Size())
     return ret, err
 }
 
-func (q *QClient) Put(path string, data []byte) (*Stat, error) {
-    stat, err := q.put(path, bytes.NewReader(data), int64(len(data)))
-    return stat, err
+//PushR 递归上传
+func (q *QClient) PushR(path string) []Stat {
+
+    stats := make([]Stat, 0)
+    var wg sync.WaitGroup
+
+    worker := func(fn string) {
+        wg.Add(1)
+        stat, err := q.PushFile(fn)
+        if err != nil {
+            return
+        }
+        stats = append(stats, *stat)
+        defer wg.Done()
+
+    }
+
+    var dfs func(dir string)
+    dfs = func(dir string) {
+
+        fs, err := ioutil.ReadDir(dir)
+        if err != nil {
+            return
+        }
+
+        for _, f := range fs {
+            if f.IsDir() {
+                dfs(dir + "/" + f.Name())
+            } else {
+                fName := dir + "/" + f.Name()
+                go worker(fName)
+            }
+        }
+    }
+
+    dfs(path)
+    wg.Wait()
+
+    return stats
+
 }
 
-func (q *QClient) reader(path string, offset int64) (io.ReadCloser, error) {
-    return nil, nil
+func (q *QClient) Reader(path string, offset int64) (io.ReadCloser, error) {
+    url := q.URLFor(path)
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return nil, err
+    }
+    if offset > 0 {
+        req.Header.Add("Range", "bytes="+strconv.FormatInt(offset, 10)+"-")
+    }
+
+    resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    if resp.StatusCode/100 != 2 {
+        return nil, errors.New(resp.Status)
+    }
+    return resp.Body, err
+}
+
+func (q *QClient) Pull(path string) ([]byte, error) {
+    reader, err := q.Reader(path, 0)
+    if err != nil {
+        return nil, err
+    }
+    defer reader.Close()
+    return ioutil.ReadAll(reader)
+}
+
+// PullAndSave 下载文件并保存到指定目录,dst="/aa/cc"
+func (q *QClient) PullTo(path, dst string) error {
+    reader, err := q.Reader(path, 0)
+    if err != nil {
+        return err
+    }
+    defer reader.Close()
+
+    err = os.MkdirAll(dst, os.ModePerm)
+    if err != nil {
+        return err
+    }
+
+    fName := q.pathBase(path)
+
+    f, err := os.Create(dst + fName)
+    if err != nil {
+        return err
+    }
+    defer f.Close()
+
+    _, err = io.Copy(f, reader)
+    return err
+}
+
+func (q *QClient) List(path string) []FileInfo {
+    fis := make([]FileInfo, 0)
+    var marker string
+    for {
+        entries, _, nextMar, hasNext, err := q.bktManager.ListFiles(q.bucket, path, "", marker, 1000)
+        if err != nil {
+            break
+        }
+
+        for _, en := range entries {
+            fi, ok := q.detail(path, &en)
+            if !ok {
+                continue
+            }
+            fis = append(fis, *fi)
+        }
+
+        if hasNext {
+            marker = nextMar
+        } else {
+            break
+        }
+    }
+    return fis
+}
+
+func (q *QClient) detail(path string, item *storage.ListItem) (*FileInfo, bool) {
+
+    fi := FileInfo{
+        Key:      item.Key,
+        Hash:     item.Hash,
+        Size:     item.Fsize,
+        PutTime:  item.PutTime,
+        MimeType: item.MimeType,
+        Type:     item.Type,
+    }
+
+    name := strings.Replace(item.Key, path, "", 1)
+    if len(name) <= 0 {
+        return nil, false
+    }
+
+    spl := strings.Split(name, "/")
+    if len(spl) <= 0 {
+        return nil, false
+    }
+
+    if len(spl) >= 2 {
+        fi.IsDir = true
+        if len(spl[0]) == 0 {
+            fi.Name = spl[1]
+        } else {
+            fi.Name = spl[0]
+        }
+    } else {
+        fi.IsDir = false
+        if len(spl[0]) == 0 {
+            return nil, false
+        } else {
+            fi.Name = spl[0]
+        }
+    }
+    return &fi, true
 
 }
--- qclient_test.go
+++ qclient_test.go
@@ -3,20 +3,22 @@ package qclient
 import (
     "testing"
     "io/ioutil"
+    "fmt"
+    "path"
+    "strings"
 )
 
 const (
     ak  = "ak"
-    sk  = "Q"
+    sk  = "sk"
     bkt = "register"
-    dm  = "data.foryung.com"
+    dm  = "http://image.foryung.com"
 )
 
+func TestQClient_PushFile(t *testing.T) {
 
-func TestQClient_PutFile(t *testing.T) {
-
-    client := NewQClient(ak, sk, bkt, dm, false, true)
-    st, err := client.PutFile("/Users/zihua/Desktop/register/test/de.gif")
+    client := NewQClient(ak, sk, bkt, dm, false, false)
+    st, err := client.PushFile("README.md")
     if err != nil {
         t.Error(err)
     } else {
@@ -24,16 +26,100 @@ func TestQClient_PutFile(t *testing.T) {
     }
 }
 
-func TestQClient_Put(t *testing.T) {
+func rprint(dir string) {
+    fs, err := ioutil.ReadDir(dir)
+    if err != nil {
+        return
+    }
+
+    for _, f := range fs {
+        if f.IsDir() {
+            rprint(dir + "/" + f.Name())
+        } else {
+            fmt.Println(dir + "/" + f.Name())
+        }
+    }
+
+}
+
+func TestQClient_PushR(t *testing.T) {
+    rprint("../qclient")
+}
+
+func TestQClient_Push(t *testing.T) {
 
     path := "/Users/zihua/Downloads/aa.mp4"
     b, _ := ioutil.ReadFile(path)
-    client := NewQClient(ak, sk, bkt, dm, false, true)
-    st, err := client.Put(path, b)
+    client := NewQClient(ak, sk, bkt, dm, false, false)
+
+    st, err := client.Push(path, b)
     if err != nil {
         t.Error(err)
     } else {
         t.Log(st)
     }
+}
 
+func TestQClient_GetURI(t *testing.T) {
+    client := NewQClient(ak, sk, bkt, dm, false, false)
+    path := "jp.gif"
+    res := client.URLFor(path)
+    t.Log(res)
+}
+
+func TestQClient_GetAndSave(t *testing.T) {
+    client := NewQClient(ak, sk, bkt, dm, false, false)
+    path := "/README.md"
+    savePath := "/Users/zihua/Desktop/register/666"
+    err := client.PullTo(path, savePath)
+    if err != nil {
+        t.Error(err)
+    } else {
+        t.Log("success")
+    }
+}
+
+func TestQClient_PushR2(t *testing.T) {
+    client := NewQClient(ak, sk, bkt, dm, false, true)
+
+    path := "/Users/zihua/Desktop"
+
+    sts := client.PushR(path)
+    fmt.Println(sts)
+}
+
+func TestQClient_PushR3(t *testing.T) {
+    fullFilename := ""
+    fmt.Println("fullFilename =", fullFilename)
+    var filenameWithSuffix string
+    filenameWithSuffix = path.Base(fullFilename) //获取文件名带后缀
+    fmt.Println("filenameWithSuffix =", filenameWithSuffix)
+    var fileSuffix string
+    fileSuffix = path.Ext(filenameWithSuffix) //获取文件后缀
+    fmt.Println("fileSuffix =", fileSuffix)
+
+    var filenameOnly string
+    filenameOnly = strings.TrimSuffix(filenameWithSuffix, fileSuffix) //获取文件名
+    fmt.Println("filenameOnly =", filenameOnly)
+}
+
+func TestQClient_List(t *testing.T) {
+    client := NewQClient(ak, sk, bkt, dm, false, true)
+    fis := client.List("/Users/zihua/Downloads/go")
+    for _, v := range fis {
+        fmt.Printf("%#v", v)
+    }
+
+}
+
+func TestQClient_PushR4(t *testing.T) {
+    v := "/Users/zihua/Downloads/go 系列/L001-Go语言-mp4/01 Go开发1期 day1 开课介绍01.mp4"
+    path := "/Users/zihua/Downloads"
+    name := strings.Replace(v, path, "", 1)
+
+    spl:=strings.Split(name,"/")
+    fmt.Println(name)
+    for _,v:=range spl{
+        fmt.Println(v)
+    }
 }
